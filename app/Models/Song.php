65319bbc6d04f7efcad7da9cc55257af3c1b4198
springhack <springhack@live.cn>
2023-11-23 00:26:01 +0800

    feat: support dynamic lyrics

--- app/Models/Song.php
+++ app/Models/Song.php
@@ -111,7 +111,8 @@ class Song extends Model
             $value = strip_tags(preg_replace('#<br\s*/?>#i', PHP_EOL, $value));
 
             // also remove the timestamps that often come with LRC files
-            return preg_replace('/\[\d{2}:\d{2}.\d{2}]\s*/m', '', $value);
+            // return preg_replace('/\[\d{2}:\d{2}.\d{2}]\s*/m', '', $value);
+            return $value;
         };
 
         return new Attribute(get: $normalizer, set: $normalizer);
